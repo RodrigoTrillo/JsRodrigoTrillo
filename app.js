9f1f0f30ab23c06bee32e422276d1e22eeb800af
Rodrigo Trillo <rollyt1232@gmail.com>
2022-08-27 12:14:38 -0300

    simulador interactivo

--- app.js
+++ app.js
@@ -1,15 +1,60 @@
-let respuesta = "buenos aires";
-let info = false;
 
-for (let contador = 0; contador <3; contador++){
-    let respuestaUsuario = prompt("Ingresa la capital de argentina");
-    if( respuestaUsuario === respuesta){
-        let info = true;
-        alert("Genial, has acertado");
-        break
+// funciones
+
+
+// funcion saludar 
+
+
+function saludo() {
+    alert("Bienvenido a PerifericosCba" +" "+ nombre + " escoja una opcion a continuacion"
+    );
+}
+
+function compraProducto() {
+    producto = prompt("Elija un producto \n 1: Auriculares \n 2: Teclados \n 3: Mouse");
+
+    if (producto === "1") {
+        alert("Elejiste Auriculares")
+    } else if ( producto === "2") {
+        alert("Elejiste Teclados")
+    } else if (producto === "3"){
+        alert("Elejiste Mouse")
     }
-    if (respuestaUsuario !==respuesta){
-        alert("Lo siento esa no es la capital, Vuelve a intenrarlo");
-        break
+    opcion = prompt("Elija que hacer a continuacion \n 2: Mostrar precio \n 3:Terminar");
+}
+
+
+function finCompra(){
+    if (producto === "1"){
+        alert(` Auriculares tiene un precio de ${auriPrecio}`)
+    } else if (producto === "2"){
+        alert(`Teclados tiene un precio de ${tecladoPrecio}`)
+    } else if (producto=== "3"){
+        alert(`Mouse tiene un precio de ${mousePrecio}`)
+    }
+
+}
+
+// variables y programa 
+
+let nombre = prompt("Porfavor ingrese su nombre")
+saludo()
+
+let producto;
+let auriPrecio = "$8000";
+let tecladoPrecio = "$5000";
+let mousePrecio = "$4000";
+
+let opcion = prompt ("Ingrese la opcion que corresponda \n 1: Comprar perifericos \n 2: Finalizar la compra \n 3: Terminar");
+
+while(opcion !== "3"){
+    if (opcion === "1"){
+        compraProducto();
+    }
+    if(opcion === "2"){
+        finCompra();
+        opcion= "3";
     }
 }
+
+alert("Gracias por comprar en PerifericosCba")
